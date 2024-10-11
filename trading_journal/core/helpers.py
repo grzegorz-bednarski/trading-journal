09f931bc39d24f39cdd88975efdbc9db55ea2560
Grzegorz Bednarski <grzegorz@bednarski.info>
2024-10-11 18:30:59 +0200

    tests and comments

--- trading_journal/core/helpers.py
+++ trading_journal/core/helpers.py
@@ -1,6 +1,2 @@
 def get_joined_m2m_names(obj, attr: str, sub_attr: str = "name") -> str:
-    return (
-        ""
-        if not hasattr(obj, attr)
-        else ", ".join(obj.markets.values_list(sub_attr, flat=True))
-    )
+    return "" if not hasattr(obj, attr) else ", ".join(obj.markets.values_list(sub_attr, flat=True))
